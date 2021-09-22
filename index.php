137afd7e52f9ffc31d5476dfaad587da2bbaee65
Lukas Toman <toman.lukas85@gmail.com>
2021-09-22 15:25:39 +0000

    Initial commit from PHPSandbox

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+
+    $jmena = ["David","Anna","Markus"];
+    echo $jmena [0];
+    echo "<br>";
+    echo $jmena [1];
+    echo "<br>";
+    echo $jmena [2];
+    echo "<br>";
+
+    //asociativni pole
+    $simpsonovi = ["Bart" => "Rostak", "Marge" =>"Modre vlasy","Homer" => "Kobliha"];
+
+    echo $simpsonovi["Bart"];
+    echo "<br>";
+    echo $simpsonovi ["Marge"];
+    echo "<br>";
+    echo $simpsonovi["Homer"];
+
+    print_r($jmena);
+
+    print_r($simpsonovi);
+
+?>
